ffad13475f35fd3ceb13d32c857c70776aa54710
rami <rami.habib@maramalabs.com>
2023-07-05 10:43:21 +1200

    feat: day 4 solution pt II

--- 2022/scripts/day4.js
+++ 2022/scripts/day4.js
@@ -6,19 +6,16 @@ function splitPair(pair) {
     .map((r) => [Number(r[0]), Number(r[1])])
 }
 
-function isFirstRangeWider(first, second) {
-  const index0= first[0] <= second[0];
-  const index1= first[1] >= second[1];
-
-  return index0 && index1
+function doesFirstRangeOverlapSecondRange(first, second) {
+  return first[1] >= second[0] && first[1] <= second[1]
 }
 
-function isOneRangeContainedInTheOther(pair) {
+function doesOneRangeOverlapTheOther(pair) {
   const ranges = splitPair(pair);
   const range1 = ranges[0];
   const range2 = ranges[1];
-  const a = isFirstRangeWider(range1, range2);
-  const b = isFirstRangeWider(range2, range1);
+  const a = doesFirstRangeOverlapSecondRange(range1, range2);
+  const b = doesFirstRangeOverlapSecondRange(range2, range1);
   return a || b
 }
 
@@ -28,13 +25,13 @@ function splitListIntoPairs(list) {
 
 function assignmentCounter(list) {
   const pairList = splitListIntoPairs(list);
-  const pairMap = pairList.map((pair) => isOneRangeContainedInTheOther(pair))
+  const pairMap = pairList.map((pair) => doesOneRangeOverlapTheOther(pair))
   return pairMap.filter((p) => p).length;
 }
 
 
 function main(pairsList) {
-  console.log('Assignment pairs where one range fully contains the other:',assignmentCounter(pairsList))
+  console.log('Assignment pairs where ranges overlap:',assignmentCounter(pairsList))
 }
 
 main(pairsList);
